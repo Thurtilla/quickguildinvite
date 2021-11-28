dca2450c0125560ddf51369db2eb7a0104e8d9cc
Thurtilla <kjetil@svergja.com>
2021-11-28 21:59:03 +0100

    Fixed issue where the guild invite would have the wrong index

--- main.lua
+++ main.lua
@@ -1,22 +1,6 @@
 -- Used to set the player to invite 
 local inviteName = ""
 
-function inviteToGuild()
-	GuildInvite(inviteName)
-end
-
-function addGuildInviteButtonToChat()
-	if CanGuildInvite() then
-		for i = 1, #UnitPopupMenus["FRIEND"] do
-			if UnitPopupMenus["FRIEND"][i] == "INVITE" then
-				tinsert(UnitPopupMenus["FRIEND"], i + 1, "GuildInvite")
-			end
-		end
-	end
-end
-
-addGuildInviteButtonToChat()
-
 local myframe = CreateFrame("Frame")
 myframe:RegisterEvent("UNIT_TARGET")
 
@@ -27,23 +11,41 @@ myframe:SetScript(
 	end
 )
 
+
+function inviteToGuild()
+	GuildInvite(inviteName)
+end
+
+function addGuildInviteButtonToChat()
+	if CanGuildInvite() then
+		for i = 1, #UnitPopupMenus["FRIEND"] do
+			if UnitPopupMenus["FRIEND"][i] == "GuildInvite" then return end
+			if UnitPopupMenus["FRIEND"][i] == "WHISPER" then
+				tinsert(UnitPopupMenus["FRIEND"], i - 1, "GuildInvite")
+				return
+			end
+		end
+	end
+end
+
+addGuildInviteButtonToChat()
+
 function myframe:UNIT_TARGET(unitid)
 	if not CanGuildInvite() then return end
 	inviteIndex = #UnitPopupMenus["PLAYER"] - 1
 	if unitid == "player" and UnitExists("target") then
 		if UnitFactionGroup("target") == UnitFactionGroup("player") and UnitIsPlayer("target") then
 			for i = 1, #UnitPopupMenus["PLAYER"] do
-				if UnitPopupMenus["PLAYER"][i] == "INVITE" then
+				if UnitPopupMenus["PLAYER"][i] == "WHISPER" then
 					inviteIndex = i
 				end
-				if UnitPopupMenus["PLAYER"][i] == "GuildInvite" or UnitPopupMenus["PLAYER"][i] == "ADD_KOS" then
-					tremove(UnitPopupMenus["PLAYER"], i)
+				if UnitPopupMenus["PLAYER"][i] == "GuildInvite" then return end
 				end
 			end
+			print("adding ginv xD")
+			tinsert(UnitPopupMenus["PLAYER"], inviteIndex - 1, "GuildInvite")
 		end
-		tinsert(UnitPopupMenus["PLAYER"], inviteIndex + 1, "GuildInvite")
 	end
-end
 
 UnitPopupButtons["GuildInvite"] = {
 	text = "Guild invite",
