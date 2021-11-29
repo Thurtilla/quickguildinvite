4ef40c367563319ceec74fe618149a954570051d
Thurtilla <kjetil@svergja.com>
2021-11-29 17:06:52 +0100

    fixed issue with multi guild invite

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+v0.3
+Fixed an issue where the guild invite would show up multiple times in the context menu
+
 v0.2
 Fixed an issue where the guild invite would be placed the wrong place in the context menu's
 
--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
--- Used to set the player to invite 
+-- Used to set the player to invite
 local inviteName = ""
+local addonName = "quickguildinvite"
 
 local myframe = CreateFrame("Frame")
 myframe:RegisterEvent("UNIT_TARGET")
+myframe:RegisterEvent("ADDON_LOADED")
 
 myframe:SetScript(
 	"OnEvent",
@@ -11,40 +13,51 @@ myframe:SetScript(
 	end
 )
 
+function myframe:ADDON_LOADED(name)
+	if name ~= addonName then
+		return
+	end
+	addGuildInviteButtonToChat()
+end
+
 function inviteToGuild()
 	GuildInvite(inviteName)
 end
 
 function addGuildInviteButtonToChat()
-	if CanGuildInvite() then
-		for i = 1, #UnitPopupMenus["FRIEND"] do
-			if UnitPopupMenus["FRIEND"][i] == "GuildInvite" then return end
-			if UnitPopupMenus["FRIEND"][i] == "WHISPER" then
-				tinsert(UnitPopupMenus["FRIEND"], i - 1, "GuildInvite")
-				return
-			end
+	if not CanGuildInvite() then
+		return
+	end
+	for i = 1, #UnitPopupMenus["FRIEND"] do
+		if UnitPopupMenus["FRIEND"][i] == "GuildInvite" then
+			return
+		end
+		if UnitPopupMenus["FRIEND"][i] == "WHISPER" then
+			tinsert(UnitPopupMenus["FRIEND"], i - 1, "GuildInvite")
+			return
 		end
 	end
 end
 
-addGuildInviteButtonToChat()
-
 function myframe:UNIT_TARGET(unitid)
-	if not CanGuildInvite() then return end
+	if not CanGuildInvite() then
+		return
+	end
 	inviteIndex = #UnitPopupMenus["PLAYER"] - 1
 	if unitid == "player" and UnitExists("target") then
 		if UnitFactionGroup("target") == UnitFactionGroup("player") and UnitIsPlayer("target") then
 			for i = 1, #UnitPopupMenus["PLAYER"] do
-				if UnitPopupMenus["PLAYER"][i] == "WHISPER" then
-					inviteIndex = i
+				if UnitPopupMenus["PLAYER"][i] == "GuildInvite" then
+					break
 				end
-				if UnitPopupMenus["PLAYER"][i] == "GuildInvite" then return end
+				if UnitPopupMenus["PLAYER"][i] == "WHISPER" then
+					tinsert(UnitPopupMenus["PLAYER"], i - 1, "GuildInvite")
+					return
 				end
 			end
-			print("adding ginv xD")
-			tinsert(UnitPopupMenus["PLAYER"], inviteIndex - 1, "GuildInvite")
 		end
 	end
+end
 
 UnitPopupButtons["GuildInvite"] = {
 	text = "Guild invite",
